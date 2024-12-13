fbeed8ba1a080b72389765bfd3be587ef1092cf5
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2024-12-13 11:32:44 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("my-btn").addEventListener("click", function(){
+    console.log("CLICKED");
+    let title = document.getElementById("title");
+    title.innerText = "Goodnight World";
+})
